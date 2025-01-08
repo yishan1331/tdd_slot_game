47e81f69e3b1435c148ddec77b52d6a36e70d401
Yishan <yishanjob13@gmail.com>
2025-01-08 12:02:55 +0800

    extract _get_screen method

--- src/slot_score_calculator.py
+++ src/slot_score_calculator.py
@@ -36,13 +36,22 @@ class SlotScoreCalculator:
             print(error)
             return str(error)
 
+    def _get_screen(self):
+        try:
+            screen = []
+            for reel in self.reels:
+                column = [reel[(self.random_num + i) % len(reel)] for i in range(3)]
+                screen.append(column)
+            return screen
+
+        except Exception as error:
+            print(error)
+            return str(error)
+
     def spin_single_reel(self, random_num):
         return [self.reels[(random_num + i) % len(self.reels)] for i in range(3)]
 
     def calculate(self, bet):
-        screen = []
-        for reel in self.reels:
-            column = [reel[(self.random_num + i) % len(reel)] for i in range(3)]
-            screen.append(column)
+        screen = self._get_screen()
         self.odd = self._get_odd(screen)
         return self.odd * bet
