ceb379153f997c5d08a318fe4f4b0451b48d16c9
Yishan <yishanjob13@gmail.com>
2025-01-08 10:14:10 +0800

    extract smaller methods

--- src/slot_score_calculator.py
+++ src/slot_score_calculator.py
@@ -10,15 +10,19 @@ class SlotScoreCalculator:
         self.odd = 0
         self.wheels = wheels
     
-    def _get_odd(self):
+    def _get_lines(self):
         try:
-            transposed = list(zip(*self.wheels))
-
             same_line = 0
-            for column in transposed:
+            for column in list(zip(*self.wheels)):
                 if len(set(column)) == 1:
                     same_line += 1
+            return same_line
 
+        except Exception as error:
+            print(error)
+    def _get_odd(self):
+        try:
+            same_line = self._get_lines()
             if same_line not in self.PAYTABLE:
                 raise RuntimeError('TBD')
             return self.PAYTABLE[same_line]
