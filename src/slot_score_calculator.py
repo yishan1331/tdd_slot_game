6de0f5773eeca2105a289585484cb8e7aa0cae27
Yishan <yishanjob13@gmail.com>
2025-01-08 09:47:11 +0800

    one line

--- src/slot_score_calculator.py
+++ src/slot_score_calculator.py
@@ -1,4 +1,16 @@
 
 class SlotScoreCalculator:
-    def calculate(self):
-        return 0
+    def __init__(self, wheels):
+        self.odd = 0
+        self.wheels = wheels
+
+    def calculate(self, bet):
+        transposed = list(zip(*self.wheels))
+
+        same_line = 0
+        for column in transposed:
+            if len(set(column)) == 1:
+                same_line += 1
+
+        if same_line == 1: self.odd = 40
+        return self.odd * bet
--- tests/test_slot_score_calculator.py
+++ tests/test_slot_score_calculator.py
@@ -3,7 +3,26 @@ from src.slot_score_calculator import SlotScoreCalculator
 
 @pytest.fixture
 def slot_score_calculator():
-    return SlotScoreCalculator()
+    def _slot_score_calculator(wheels):
+        return SlotScoreCalculator(wheels)
+    return _slot_score_calculator
 
 def test_lose(slot_score_calculator):
-    assert slot_score_calculator.calculate() == 0
+    wheels = [
+        ['A', '1', '2'],
+        ['A', '1', '2'],
+        ['A', '1', '2'],
+        ['A', '1', '2'],
+        ['1', '2', '3'],
+    ]
+    assert slot_score_calculator(wheels).calculate(10) == 0
+
+def test_one_line(slot_score_calculator):
+    wheels = [
+        ['A', '1', '2'],
+        ['A', '1', '2'],
+        ['A', '1', '2'],
+        ['A', '1', '2'],
+        ['A', '2', '3'],
+    ]
+    assert slot_score_calculator(wheels).calculate(10) == 400
