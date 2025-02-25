c4f905ca1ea76efdf74e28c957ad5e4d99e469cc
Yishan <yishanjob13@gmail.com>
2025-02-25 13:47:26 +0800

    DI random to Reels class

--- src/reels.py
+++ src/reels.py
@@ -1,14 +1,15 @@
 import random
 
 class Reels:
-    def __init__(self, raw_reels):
+    def __init__(self, raw_reels, random_num):
         self.raw_reels = raw_reels
+        self.random_num = random_num
 
-    def reels_to_screen(self, random_num):
+    def reels_to_screen(self):
         try:
             screen = []
             for reel in self.raw_reels:
-                column = [reel[(random_num + i) % len(reel)] for i in range(3)]
+                column = [reel[(self.random_num + i) % len(reel)] for i in range(3)]
                 screen.append(column)
             return screen
 
--- src/slot_score_calculator.py
+++ src/slot_score_calculator.py
@@ -6,15 +6,14 @@ class SlotScoreCalculator:
     
     def __init__(self, pay_table=None, raw_reels=None, random_num=None):
         self.odd = 0
-        self.reels = Reels(raw_reels)
-        self.random_num = random_num
         self.pay_table = pay_table
+        self.reels = Reels(raw_reels, random_num)
 
     def spin_single_reel(self, random_num):
         return [self.reels.raw_reels[(random_num + i) % len(self.reels.raw_reels)] for i in range(3)]
 
     def calculate(self, bet):
-        raw_screen = self.reels.reels_to_screen(self.random_num)
+        raw_screen = self.reels.reels_to_screen()
         screen = Screen(raw_screen)
         self.odd = self.pay_table.get_odd(screen)
         return self.odd * bet
