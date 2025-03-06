ecfe4c9b79038563f8263ce67b8014950a6b8e74
Yishan <yishanjob13@gmail.com>
2025-03-06 10:33:43 +0800

    extract delegate: RNG

--- src/random_number_generator.py
+++ src/random_number_generator.py
@@ -1,6 +1,23 @@
+import random
 class RandomNumberGenerator:
-    def __init__(self, random_num=None):
+    def __init__(self, random_num, raw_reels):
         self.random_num = random_num
-    
-    def next_random_int(self, bound):
-        return
+
+        bound = len(raw_reels)
+        if random_num is None:
+            self.random_num = [self._next_random_int(bound - 1) for _ in range(bound)]
+        else:
+            if isinstance(random_num, int):
+                self.random_num = [random_num] * bound
+            elif isinstance(random_num, list):
+                if len(random_num) > bound:
+                    self.random_num = random_num[:bound]
+                elif len(random_num) < bound:
+                    self.random_num = random_num+[self._next_random_int(bound - 1)]*(bound-len(random_num))
+
+    def _next_random_int(self, bound):
+        return random.randint(0, bound)
+
+    def gen_random_list(self):
+        return self.random_num
+        
--- src/reels.py
+++ src/reels.py
@@ -1,22 +1,10 @@
-import random
 from .screen import Screen
 from .random_number_generator import RandomNumberGenerator
 
 class Reels:
     def __init__(self, raw_reels, random_num=None):
         self.raw_reels = raw_reels
-        self.random_num = random_num
-        RNG = RandomNumberGenerator()
-        if random_num is None:
-            self.random_num = [random.randint(0, RNG.next_random_int(len(self.raw_reels) - 1)) for _ in raw_reels]
-        else:
-            if isinstance(random_num, int):
-                self.random_num = [random_num] * len(self.raw_reels)
-            elif isinstance(random_num, list):
-                if len(random_num) > len(self.raw_reels):
-                    self.random_num = random_num[:len(self.raw_reels)]
-                elif len(random_num) < len(self.raw_reels):
-                    self.random_num = random_num+[random.randint(0, len(self.raw_reels) - 1)]*(len(self.raw_reels)-len(random_num))
+        self.random_num = RandomNumberGenerator(random_num, raw_reels).gen_random_list()
 
     def reels_to_screen(self):
         raw_screen = []
